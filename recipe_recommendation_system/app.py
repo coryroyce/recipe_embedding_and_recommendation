7d591b5aad74e49b0c06fc05f94986d0b77fe102
waviousmontavious <montesdavid88@gmail.com>
2023-04-12 21:02:48 -0700

    added preliminary recipe page with substitution options

--- recipe_recommendation_system/app.py
+++ recipe_recommendation_system/app.py
@@ -13,25 +13,37 @@ ingredient_embedding.run_prep_process()
 cur_query = ''
 
 @app.route('/', methods=['GET', 'POST'])
-def search():
+def index():
     if request.method == 'POST':
         data = dict(request.form)
-        results = ingredient_embedding.query_semantic_index_recipe_titles(query=data['search'])
+        indices = ingredient_embedding.query_semantic_index_recipe_titles(query=data['search'])
+        results = ingredient_embedding.df_recipe.iloc[indices]['title'].tolist()
         cur_query = data['search']
     else:
         cur_query = ''
         results = []
-    return render_template('index.html', recipes=results, cur_query=cur_query)
+        indices = []
+    return render_template('index.html', recipes=results, cur_query=cur_query, indices=indices)
 
 
 @app.route('/recipes/<int:index>')
 def show_recipe(index):
     recipe = ingredient_embedding.df_recipe.iloc[index]
-    print(recipe)
+    title = recipe['title']
+    directions = '\n'.join(json.loads(recipe['directions']))
+    ingredients = '\n'.join(json.loads(recipe['ingredients_with_measurements']))
+    ingredient_ents = json.loads(recipe['ingredients'])
+
+    sub_ingredients = lambda q: ingredient_embedding.query_semantic_index_ingredients(query=q)
+    data = {ent: sub_ingredients(ent) for ent in ingredient_ents}
+    data_str = json.dumps(data, indent=4)
+    
+    return render_template('recipe.html', title=title, directions=directions, ingredients=ingredients, subs=data_str)
+
 
 
 @app.route('/search', methods=['GET', 'POST'])
-def index():
+def search():
     if request.method == 'POST':
         data = dict(request.form)
         recipes = get_recipe_by_title(data['search'])
